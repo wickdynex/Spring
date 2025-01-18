a4d867258594f94abb53f256e034e9a9f340ac18
YiXuan Ding <1328032567@qq.com>
2025-01-18 20:54:18 +0800

    <add>: add main file.

---
+++ src/main/java/com/summer/Test.java
@@ -0,0 +1,11 @@
+package com.summer;
+
+import com.spring.ApplicationContext;
+
+public class Test {
+
+    public static void main(String[] args) {
+        ApplicationContext context = new ApplicationContext(ApplicationConfig.class);
+
+    }
+}
