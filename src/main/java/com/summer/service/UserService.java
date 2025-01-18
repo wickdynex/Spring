4d528a783990a59f4901fda221d001f9f0098d14
YiXuan Ding <1328032567@qq.com>
2025-01-18 22:02:03 +0800

    <modify>: update UserService.

--- src/main/java/com/summer/service/UserService.java
+++ src/main/java/com/summer/service/UserService.java
@@ -1,7 +1,15 @@
 package com.summer.service;
 
+import com.spring.Autowired;
 import com.spring.Component;
 
 @Component
 public class UserService {
+
+    @Autowired
+    private OrderService orderService;
+
+    public void test() {
+        System.out.println(orderService);
+    }
 }
