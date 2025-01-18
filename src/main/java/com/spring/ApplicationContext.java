3ef47d4c54536fac5a7ee6481cde35316ef57d1f
YiXuan Ding <1328032567@qq.com>
2025-01-18 21:46:12 +0800

    <add>: add new method.

--- src/main/java/com/spring/ApplicationContext.java
+++ src/main/java/com/spring/ApplicationContext.java
@@ -1,14 +1,50 @@
 package com.spring;
 
 import java.io.File;
+import java.lang.reflect.InvocationTargetException;
 import java.net.URL;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class ApplicationContext {
     private Class configClass;
 
+    private ConcurrentHashMap<String, Object> singletonsObjects = new ConcurrentHashMap<>();
+
+    private ConcurrentHashMap<String, BeanDefinition> beanDefinitionMap = new ConcurrentHashMap<>();
+
     public ApplicationContext(Class configClass) {
         this.configClass = configClass;
-        if(configClass.isAnnotationPresent(ComponentScan.class)) {
+        scan(configClass);
+        for(String beanName : beanDefinitionMap.keySet()) {
+            BeanDefinition beanDefinition = beanDefinitionMap.get(beanName);
+            if(beanDefinition.getBeanScope().equals(BeanScope.singleton.getScope())) {
+                Object bean = createBean(beanDefinition);
+                singletonsObjects.put(beanName, bean);
+            }
+            else {
+
+            }
+        }
+    }
+
+    public Object createBean(BeanDefinition beanDefinition) {
+        Class<?> beanClass = beanDefinition.getBeanClass();
+        try {
+            Object instance = beanClass.getDeclaredConstructor().newInstance();
+            return instance;
+        } catch (InstantiationException e) {
+            throw new RuntimeException(e);
+        } catch (IllegalAccessException e) {
+            throw new RuntimeException(e);
+        } catch (InvocationTargetException e) {
+            throw new RuntimeException(e);
+        } catch (NoSuchMethodException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private void scan(Class configClass) {
+        if (configClass.isAnnotationPresent(ComponentScan.class)) {
             ComponentScan componentScanAnnotation = (ComponentScan) configClass.getDeclaredAnnotation(ComponentScan.class);
             String path = componentScanAnnotation.value();
             path = path.replace(".", "/");
@@ -18,17 +54,33 @@ public class ApplicationContext {
             if (file.isDirectory()) {
                 File[] files = file.listFiles();
                 for (File f : files) {
-                    System.out.println(f);
+//                    System.out.println(f);
                     String fileName = f.getAbsolutePath();
-                    if(fileName.endsWith(".class"))  {
+                    if (fileName.endsWith(".class")) {
                         String classPath = fileName.substring(fileName.indexOf("com"), fileName.indexOf(".class"));
-                        System.out.println(classPath);
-                        classPath = classPath.replace("\\", ".");;
-                        System.out.println(classPath);
+//                        System.out.println(classPath);
+                        classPath = classPath.replace("\\", ".");
+//                        System.out.println(classPath);
                         try {
                             Class<?> clazz = classLoader.loadClass(classPath);
                             if (clazz.isAnnotationPresent(Component.class)) {
-                                // create a bean
+                                // set bean name to beanDefinationMap
+                                Component componentAnnotation = clazz.getAnnotation(Component.class);
+                                String beanName = componentAnnotation.value();
+                                if (beanName.isEmpty()) {
+                                    beanName = clazz.getSimpleName();
+                                    beanName = beanName.substring(0, 1).toLowerCase() + beanName.substring(1);
+                                }
+//                                System.out.println(beanName);
+
+                                BeanDefinition beanDefinition = new BeanDefinition();
+                                beanDefinition.setBeanClass(clazz);
+                                beanDefinition.setBeanScope(BeanScope.singleton.getScope());
+                                if (clazz.isAnnotationPresent(Scope.class)) {
+                                    String beanScope = clazz.getAnnotation(Scope.class).value();
+                                    beanDefinition.setBeanScope(beanScope);
+                                }
+                                beanDefinitionMap.put(beanName, beanDefinition);
                             }
                         } catch (ClassNotFoundException e) {
                             throw new RuntimeException(e);
@@ -41,6 +93,21 @@ public class ApplicationContext {
     }
 
     public Object getBean(String beanName) {
-        return null;
+        if (beanDefinitionMap.containsKey(beanName)) {
+            BeanDefinition beanDefinition = beanDefinitionMap.get(beanName);
+            String beanScope = beanDefinition.getBeanScope();
+            Object object;
+            if(beanScope.equals(BeanScope.singleton.getScope())){
+                 object = singletonsObjects.get(beanName);
+            }
+            else {
+                // create a new bean
+                object = createBean(beanDefinition);
+            }
+            return object;
+        }
+        else {
+            throw new NullPointerException("Bean not found: " + beanName);
+        }
     }
 }
